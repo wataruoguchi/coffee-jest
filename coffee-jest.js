258ee1d77d39a00474f5da17f3528fc31a93f02d
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2017-12-10 23:26:48 -0800

    s/es2015/env/

--- coffee-jest.js
+++ coffee-jest.js
@@ -2,21 +2,23 @@ const coffee = require('coffeescript')
 const crypto = require('crypto')
 
 module.exports = {
-    process: (src, path) => {
-        const script = coffee.compile(
-            src,
-            {
-                bare: true,
-                transpile: {
-                    presets: ['es2015']
-                }
-            }
-        )
-        return {
-            code: script
+  process: (src, path) => {
+    const script = coffee.compile(
+      src,
+      {
+        bare: true,
+        sourceMap: true,
+        transpile: {
+          presets: ['env']
         }
-    },
-    getCacheKey: (fileData, filename, configString) => {
-        return crypto.createHash('md5').update(fileData + filename + configString, 'utf8').digest('hex')
+      }
+    )
+    return {
+      code: script.js,
+      map: script.v3SourceMap
     }
+  },
+  getCacheKey: (fileData, filename, configString) => {
+    return crypto.createHash('md5').update(fileData + filename + configString, 'utf8').digest('hex')
+  }
 }
